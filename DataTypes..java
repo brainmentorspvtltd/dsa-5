218c367ba1aff95d643bcab283a946564a432f56
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-06-02 15:53:34 +0530

    Add files via upload

---
+++ DataTypes..java
@@ -0,0 +1,19 @@
+class DataTypes {
+    public static strictfp void main(String[] args) {
+        // int fnum = 10;
+        // int snum = 20;
+        int _fnum = 10;
+        int _snum = 10;
+        int result = _fnum + _snum;
+        System.out.println("Sum is " + result);
+
+        int $num = 100;
+        long distInMeters = 1200L;
+
+        float x = 10.5f;
+        double x1 = 55.55;
+
+        char msg = 'H'; //2 bytes
+        boolean flag = true;
+    }
+}
