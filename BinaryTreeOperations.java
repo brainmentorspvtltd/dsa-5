ae62c7d2f99518ec32e4abf5ee5a7c6cfc4d22e5
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-07-14 15:51:19 +0530

    Add files via upload

--- BinaryTreeOperations.java
+++ BinaryTreeOperations.java
@@ -132,7 +132,43 @@ public class BinaryTreeOperations {
 		System.out.println(root.data);
 	}
 	
-	public void postOrderIter() {
+	public void postOrderIter(BinaryTree<Integer> root) {
+		Stack<BinaryTree> stack = new Stack<BinaryTree>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		if(root == null) {
+			return;
+		}
+		stack.push(root);
+		BinaryTree<Integer> prev = null;
+		while(!stack.isEmpty()) {
+			BinaryTree<Integer> currentNode = stack.peek();
+			if(prev == null || prev.left == currentNode || prev.right == currentNode) {
+				if(currentNode.left != null) {
+					stack.push(currentNode.left);
+				}
+				else if(currentNode.right != null) {
+					stack.push(currentNode.right);
+				}
+				else {
+					stack.pop();
+					list.add(currentNode.data);
+				}
+			}
+			else if(currentNode.left == prev) {
+				if(currentNode.right != null) {
+					stack.push(currentNode.right);
+				}
+				else {
+					stack.pop();
+					list.add(currentNode.data);
+				}
+			}
+			else if(currentNode.right == prev) {
+				stack.pop();
+				list.add(currentNode.data);
+			}
+			prev = currentNode;
+		}
 		
 	}
 	
@@ -283,7 +319,112 @@ public class BinaryTreeOperations {
 		int distance = 0;
 		verticalOrder(root, distance, map);
 		for(Entry<Integer, ArrayList<Integer>> m : map.entrySet()) {
-			System.out.println(m.getKey() + " : " + m.getValue().get(0));
+			System.out.println(m.getKey() + " : " + m.getValue().get(m.getValue().size() - 1));
+		}
+	}
+	
+	public void diagonalView(BinaryTree<Integer> root) {
+		LinkedList<BinaryTree<Integer>> queue = new LinkedList<BinaryTree<Integer>>();
+		queue.add(root);
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			while(size > 0) {
+				BinaryTree<Integer> current = queue.removeFirst();
+				while(current != null) {
+					System.out.println(current.data + ", ");
+					if(current.left != null) {
+						queue.addLast(current.left);
+					}
+					current = current.right;
+				}
+				System.out.println();
+			}
+		}
+	}
+	
+	private void printLeftBoundary(BinaryTree<Integer> root) {
+		if(root == null) {
+			return;
+		}
+		if(root.left != null) {
+			System.out.println(root.data);
+			printLeftBoundary(root.left);
+		}
+		else if(root.right != null) {
+			System.out.println(root.data);
+			printLeftBoundary(root.right);
+		}
+	}
+	
+	private void printRightBoundary(BinaryTree<Integer> root) {
+		if(root == null) {
+			return;
+		}
+		if(root.right != null) {
+			System.out.println(root.data);
+			printRightBoundary(root.right);
+		}
+		else if(root.left != null) {
+			System.out.println(root.data);
+			printRightBoundary(root.left);
+		}
+	}
+	
+	private void printLeaf(BinaryTree<Integer> root) {
+		if(root == null) {
+			return;
+		}
+		printLeaf(root.left);
+		if(root.left == null && root.right == null) {
+			System.out.print(root.data);
+		}
+		printLeaf(root.right);
+	}
+	
+	public void printBoundary(BinaryTree<Integer> root) {
+		System.out.println(root.data);
+		printLeftBoundary(root.left);
+		System.out.println();
+		printLeaf(root);
+		System.out.println();
+		printRightBoundary(root.right);
+	}
+	
+	boolean isCorrectSum(BinaryTree<Integer> root) {
+		if(root == null) {
+			return true;
+		}
+		if(root.left == null && root.right == null) {
+			return true;
+		}
+		int sum = 0;
+		if(root.left != null) {
+			sum += root.left.data;
+		}
+		if(root.right != null) {
+			sum += root.right.data;
+		}
+		
+		return (root.data == sum && isCorrectSum(root.left) && isCorrectSum(root.right));	
+	}
+	
+	public int isBalanced(BinaryTree<Integer> root) {
+		if(root == null) {
+			return 0;
+		}
+		int leftH = isBalanced(root.left);
+		if(leftH == -1) {
+			return -1;
+		}
+		int rightH = isBalanced(root.right);
+		if(rightH == -1) {
+			return -1;
+		}
+		if(Math.abs(leftH - rightH) > 1) {
+			return -1;
+		}
+		else {
+			return Math.max(leftH, rightH) + 1;
 		}
 	}
 	
@@ -305,6 +446,10 @@ public class BinaryTreeOperations {
 		obj.printVerticalOrder(root);
 		System.out.println("===================");
 		obj.topView(root);
+		System.out.println("===================");
+		obj.bottomView(root);
+		System.out.println("===================");
+		obj.isBalanced(root);
 	}
 
 }
